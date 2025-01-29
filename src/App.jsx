72baf15ba7104bcc909ecdcab76420fb8a805312
Tokoloho Lekoro <tokoloho57@gmail.com>
2025-01-29 14:57:48 +0200

    Fixed searchbar import

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import './App.css'
-import SearchBar from './searchBar'
+import SearchBar from './Components/SearchBar/Searchbar'
 
 function App() {
   const [movies, setMovies] = useState([])
