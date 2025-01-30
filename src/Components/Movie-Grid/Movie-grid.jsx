7f15902fab4a1e2a597d3eaaa14f95755673b2de
Tokoloho Lekoro <tokoloho57@gmail.com>
2025-01-30 08:23:08 +0200

    Commented out movie details

--- src/Components/Movie-Grid/Movie-grid.jsx
+++ src/Components/Movie-Grid/Movie-grid.jsx
@@ -12,14 +12,14 @@ const MovieGrid = () => {
   return (
     <div className="movies-grid">
       {movies.map((movie) => (
-        <div key={movie.id} className="movie-card">
+        <div key={movie.id} className="movie-card-container">
           <img
             src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
             alt={movie.title}
           />
-          <h2>{movie.title}</h2>
+          {/* <h2>{movie.title}</h2>
           <p>{movie.overview}</p>
-          <p>Rating: {movie.vote_average.toFixed(1)}/10</p>
+          <p>Rating: {movie.vote_average.toFixed(1)}/10</p> */}
         </div>
       ))}
     </div>
