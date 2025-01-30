50cc7ed5fb34c4be2691a31a2bd20d9c67a40b8e
Tokoloho Lekoro <tokoloho57@gmail.com>
2025-01-30 08:51:23 +0200

    added movie-card.jsx and routing

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,23 @@ import './App.css'
 import SearchBar from './Components/SearchBar/Searchbar'
 import MovieGrid from './Components/Movie-Grid/Movie-grid'
 import { MovieProvider } from './MovieContext'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
     <MovieProvider>
-      <div className="app">
-        <h1>Nexus Movies</h1>
-        <SearchBar />
-        <MovieGrid />
-      </div>
+      <Router>
+        <div className="app">
+          <h1>Nexus Movies</h1>
+          <SearchBar />
+          <Routes>
+            <Route path="/" element={<MovieGrid />} />
+            <Route path="/movie/:id" element={<MovieDetails />} />
+          </Routes>
+        </div>
+      </Router>
     </MovieProvider>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/Components/Movie=Card/Movie-Card.jsx
+++ src/Components/Movie=Card/Movie-Card.jsx
@@ -1,9 +1,8 @@
 import React from 'react';
-import './Movie-Grid.css';
+import './Movie-Card.css';
 import { useGlobalContext } from '../../MovieContext';
-import { Link } from 'react-router-dom';
 
-const MovieGrid = () => {
+const MovieCard = () => {
   const { movies, loading } = useGlobalContext();
 
   if (loading) {
@@ -14,15 +13,11 @@ const MovieGrid = () => {
     <div className="movies-grid">
       {movies.map((movie) => (
         <div key={movie.id} className="movie-card">
-          <Link to={`/movie/${movie.id}`}>
-            <img
-              src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
-              alt={movie.title}
-            />
-          </Link>
-          <h2>
-            <Link to={`/movie/${movie.id}`}>{movie.title}</Link>
-          </h2>
+          <img
+            src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+            alt={movie.title}
+          />
+          <h2>{movie.title}</h2>
           <p>{movie.overview}</p>
           <p>Rating: {movie.vote_average.toFixed(1)}/10</p>
         </div>
@@ -31,4 +26,4 @@ const MovieGrid = () => {
   );
 };
 
-export default MovieGrid;
+export default MovieCard;
