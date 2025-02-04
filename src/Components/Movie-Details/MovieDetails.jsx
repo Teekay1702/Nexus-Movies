674f3eeeeccc56a4310f1bf5668028bafa69d450
kelebohile <kelebohilemokone@gmail.com>
2025-02-04 09:43:59 +0200

    removed extra use params

--- src/Components/Movie-Details/MovieDetails.jsx
+++ src/Components/Movie-Details/MovieDetails.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useParams, Link, useNavigate } from 'react-router-dom';
 import { useGlobalContext } from '../../MovieContext';
-import { useParams, Link } from 'react-router-dom';
 import './MovieDetails.css'
 
 
