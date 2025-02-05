e9a0b103b14208b0ed38497355d425bee71216e9
Phumlani-Mabena <phumlanimabena2@gmail.com>
2025-02-05 09:25:07 +0200

    refactor MovieDetails component to reorganize movie information display

--- src/Components/Movie-Details/MovieDetails.jsx
+++ src/Components/Movie-Details/MovieDetails.jsx
@@ -86,14 +86,7 @@ const MovieDetails = () => {
           🏠 Back to Home
         </button>
       </div>
-      <img
-        src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
-        alt={movie.title}
-      />
-      <h2>{movie.title}</h2>
-      <p>{movie.overview}</p>
-      <p>Rating: {movie.vote_average.toFixed(1)}/10</p>
-      <p className='release-date'>Release Date: {movie.release_date}</p>
+
       {/* YouTube Trailer */}
       {trailer ? (
         <div className="trailer">
@@ -111,6 +104,16 @@ const MovieDetails = () => {
       ) : (
         <p>No trailer available.</p>
       )}
+      
+      <img
+        src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+        alt={movie.title}
+      />
+      <h2>{movie.title}</h2>
+      <p>{movie.overview}</p>
+      <p>Rating: {movie.vote_average.toFixed(1)}/10</p>
+      <p className='release-date'>Release Date: {movie.release_date}</p>
+      
     </div>
   );
 };
