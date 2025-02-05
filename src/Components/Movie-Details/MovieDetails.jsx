daa4af7fac6d41877cf260a130489df5c3c2d2f8
Phumlani-Mabena <phumlanimabena2@gmail.com>
2025-02-05 09:26:33 +0200

    update MovieDetails component to change rating label to 'IMBD Ratings'

--- src/Components/Movie-Details/MovieDetails.jsx
+++ src/Components/Movie-Details/MovieDetails.jsx
@@ -104,14 +104,14 @@ const MovieDetails = () => {
       ) : (
         <p>No trailer available.</p>
       )}
-      
+
       <img
         src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
         alt={movie.title}
       />
       <h2>{movie.title}</h2>
       <p>{movie.overview}</p>
-      <p>Rating: {movie.vote_average.toFixed(1)}/10</p>
+      <p> IMBD Ratings: {movie.vote_average.toFixed(1)}/10</p>
       <p className='release-date'>Release Date: {movie.release_date}</p>
       
     </div>
